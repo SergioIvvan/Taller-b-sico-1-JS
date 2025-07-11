2e929afa2aa9d24a1a3f05f396d628f6020c5be8
Sergioivvan <sergioivano9385@gmail.com>
2025-07-11 12:24:50 -0500

    Modificado 2

--- Ejercicio1.js
+++ Ejercicio1.js
@@ -3,3 +3,6 @@ function calcularPropina(total, porcentaje){
   let montoFinal = total + propina;
   return montoFinal;
 }
+let cuenta = 500000;
+let porcentajePropina = 20;
+console.log("Monto total a pagar:", calcularPropina(cuenta, porcentajePropina));
